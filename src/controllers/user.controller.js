250a8225c249cf953f457d7db6e9da48e2c1fdbc
M Zubair <md.zubair143598@gmail.com>
2024-11-19 23:11:42 +0500

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //1. get details from user
   const { fullName, email, username, password } = req.body;
-  console.log("email:", email);
+  // console.log("email:", email);
 
   // we can check like this validation, if we have many field than we will write same like  if condition for other field like username, password etc
 
@@ -47,17 +47,34 @@ const registerUser = asyncHandler(async (req, res) => {
   //3. check if user already exist check through: username, email
   // User.findOne({email})
   // we can check like this but we have to check  both the  email and username
-  const existedUser = User.findOne({
+  const existedUser =await User.findOne({
     $or: [{ email }, { username }],
   });
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exist ");
   }
 
+  console.log(req.files)
+ 
+
+
   //4. check for images, check for avatar
   // as like req.body is given by express same like file access is given by middleware
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0].path
+  //chai aur code approach
+  let coverImageLocalPath ;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
+  {
+    coverImageLocalPath = req.files?.coverImage[0].path
+  }
+
+
+// my approach to solve the problem if there is no cover image than it don't through error for undefine 
+  // if(req.files?.coverImage)
+  // {
+  //   coverImageLocalPath= req.files?.coverImage[0]?.path;
+  // }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -93,7 +110,7 @@ if(!createdUser){
 //9. return response
 
 return res.status(201).json(
-  new ApiResponse(200, createdUser, user Register successfully)
+  new ApiResponse(200, createdUser, "user Register successfully")
 )
 
 });
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -22,7 +22,10 @@ const uploadOnCloudinary = async ( localFilePath)=>{
         })
 
         //file has been successfully uploaded 
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+
+        fs.unlinkSync(localFilePath)
+
         return response        
 
     } catch (error) {
